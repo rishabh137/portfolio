38695e3cd7646363eb927b1f0fc186a9e3f70d64
Rishabh Raj <rishabhraj13951@gmail.com>
2024-07-11 12:26:27 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <>
-      app
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-scroll"
+
+const Navbar = () => {
+    return (
+        <div id="header">
+            <div className="container">
+                <nav>
+                    <h1 className="logo"><span>R</span>ishabh.</h1>
+                    <ul id="sidemenu">
+                        <li>
+                            <Link to="/" smooth={true} duration={500}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to="about" smooth={true} duration={500}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="services" smooth={true} duration={500}>Services</Link>
+                        </li>
+                        <li>
+                            <Link to="portfolio" smooth={true} duration={500}>Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
